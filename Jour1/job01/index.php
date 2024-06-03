d2c76df6b15575fe50cac45ec07e7928de0875ad
Tom COLEGNO <tomcolegno@Toms-Laptop.local>
2024-06-03 10:19:50 +0200

    day1 job01 done

---
+++ Jour1/job01/index.php
@@ -0,0 +1,20 @@
+<?php
+$plateforme = "LaPlateforme";
+echo $plateforme . "<br>";
+
+$vive = "Vive";
+$exclamation = "!";
+echo $vive . " " . $plateforme . " " . $exclamation . "<br>";
+
+$number = 6;
+echo $number . "<br>";
+
+$number += 4;
+echo $number . "<br>";
+
+$bool = true;
+echo var_export($bool, true) . "<br>";
+
+$bool = false;
+echo var_export($bool, true) . "<br>";
+?>
